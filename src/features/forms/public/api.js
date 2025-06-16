09572cdfd74ac3ffc02a4703e4edcda51e28b58d
Jose Mejia Martinez <josemejiamartinez@MacBook-Air-de-Jose.local>
2025-06-16 16:25:00 -0500

    .

--- src/features/forms/public/api.js
+++ src/features/forms/public/api.js
@@ -3,18 +3,18 @@ import { axiosInstance } from '../../../services/fetch';
 
 export const findById = async (id) => {
     try {
-        const res = await axiosInstance("https://questionask-form-viewer.onrender.com").get(`/v1/api/public/forms/${id}`);
+        const res = await axiosInstance("https://questionask-form-viewer-ms.onrender.com").get(`/v1/api/public/forms/${id}`);
         console.log("response", res.data);
         return res.data;
     } catch (error) {
         console.log("err", error);
-        throw error;
+        throw error; 
     }
 }
 
 export const saveResponse = async (payload) => {
     try {
-        const res = await axiosInstance("https://questionask-form-viewer.onrender.com").post(`/v1/api/responses`, payload);
+        const res = await axiosInstance("https://questionask-form-viewer-ms.onrender.com").post(`/v1/api/responses`, payload);
         console.log("response", res.data);
         return res.data;
     } catch (error) {
