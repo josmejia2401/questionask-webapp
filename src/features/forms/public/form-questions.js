b9289bfd5deb408251fd945689df107f1a89f5f0
Jose Mejia Martinez <josemejiamartinez@MacBook-Air-de-Jose.local>
2025-06-17 14:07:13 -0500

    Estadisticas

--- src/features/forms/public/form-questions.js
+++ src/features/forms/public/form-questions.js
@@ -144,11 +144,29 @@ export default function FormQuestions() {
         }
         return true;
     };
-
     if (loading) {
         return (
             <div className="flex justify-center items-center h-64">
-                <div className="text-indigo-600 animate-spin h-10 w-10 border-4 border-current border-t-transparent rounded-full"></div>
+                <svg
+                    className="animate-spin h-10 w-10 text-indigo-600"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                >
+                    <circle
+                        className="opacity-25"
+                        cx="12"
+                        cy="12"
+                        r="10"
+                        stroke="currentColor"
+                        strokeWidth="4"
+                    ></circle>
+                    <path
+                        className="opacity-75"
+                        fill="currentColor"
+                        d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+                    ></path>
+                </svg>
             </div>
         );
     }
